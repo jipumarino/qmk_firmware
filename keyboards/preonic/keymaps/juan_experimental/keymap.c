e01e6cb335b75faffc39c8f4498947c33c3c0ccf
Juan Pumarino <juan@scribd.com>
2019-12-30 18:07:00 -0800

    Better numpad

--- keyboards/preonic/keymaps/juan_experimental/keymap.c
+++ keyboards/preonic/keymaps/juan_experimental/keymap.c
@@ -22,15 +22,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     CTR_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_ENT,
-    KC_HYPR, SPECIAL, KC_LALT, KC_LGUI, LOWER  , KC_SPC , KC_SPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
+    KC_HYPR, SPECIAL, KC_LALT, KC_LGUI, RAISE  , KC_SPC , KC_SPC , LOWER  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
   ),
 
   [_LOWER] = LAYOUT_preonic_grid(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_GRV , _______, _______, _______, _______, _______, _______, _______, _______, KC_P7  , KC_P8  , KC_P9  ,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P4  , KC_P5  , KC_P6  ,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P1  , KC_P2  , KC_P3  ,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0  , KC_PDOT, KC_ENT
+    KC_GRV , _______, _______, _______, _______, _______, _______, KC_PEQL, KC_PPLS, KC_P7  , KC_P8  , KC_P9  ,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PMNS, KC_P4  , KC_P5  , KC_P6  ,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PAST, KC_P1  , KC_P2  , KC_P3  ,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_P0  , KC_PDOT, KC_PENT
   ),
 
 
