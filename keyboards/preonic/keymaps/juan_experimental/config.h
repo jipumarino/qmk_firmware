6358d8d74ae7d100d193efe51eb4fba254e02f06
Juan Pumarino <juan@scribd.com>
2020-01-02 12:14:50 -0800

    Lower noise

--- keyboards/preonic/keymaps/juan_experimental/config.h
+++ keyboards/preonic/keymaps/juan_experimental/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-  #define STARTUP_SONG SONG(S__NOTE(_G5))
-  #define GOODBYE_SONG SONG(S__NOTE(_G5))
+  #define STARTUP_SONG SONG(S__NOTE(_G4))
+  #define GOODBYE_SONG SONG(S__NOTE(_G4))
 #endif
