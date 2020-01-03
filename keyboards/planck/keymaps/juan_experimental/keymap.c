364a4ce8fa2ba030a2d2067dc2672daeb2597243
Juan Pumarino <juan@scribd.com>
2020-01-02 16:32:25 -0800

    replicate to planck

--- keyboards/planck/keymaps/juan_experimental/keymap.c
+++ keyboards/planck/keymaps/juan_experimental/keymap.c
@@ -2,36 +2,38 @@
 
 enum planck_layers {
   _BASE,
-  _LOWER,
+  _NUMBR,
   _RAISE,
   _ADJUST,
   _SPECIAL,
 };
 
-#define LOWER MO(_LOWER)
+#define NUMBR MO(_NUMBR)
 #define RAISE MO(_RAISE)
 #define SPECIAL MO(_SPECIAL)
 
 #define CTR_ESC CTL_T(KC_ESC)
+#define HYP_TAB HYPR_T(KC_TAB)
+#define LWR_TAB LT(_NUMBR, KC_TAB)
 #define SFT_ENT SFT_T(KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+    LWR_TAB, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     CTR_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_ENT,
-    KC_HYPR, SPECIAL, KC_LALT, KC_LGUI, LOWER  , KC_SPC , KC_SPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
+    KC_HYPR, SPECIAL, KC_LALT, KC_LGUI, RAISE  , KC_SPC , KC_SPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
   ),
 
-  [_LOWER] = LAYOUT_planck_grid(
-    KC_GRV , _______, _______, _______, _______, _______, KC_7   , KC_8   , KC_9   , KC_PEQL, _______, KC_DEL,
-    KC_PIPE, _______, _______, _______, _______, _______, KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PAST, _______,
-    _______, _______, _______, _______, _______, _______, KC_1   , KC_2   , KC_3   , KC_PMNS, KC_PSLS, KC_PENT,
-    _______, _______, _______, _______, _______, KC_0   , KC_0   , KC_PDOT, _______, _______, _______, _______
+  [_NUMBR] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, KC_PPLS, KC_7   , KC_8   , KC_9   , _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_PMNS, KC_4   , KC_5   , KC_6   , _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_PAST, KC_1   , KC_2   , KC_3   , KC_PENT,
+    _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_0   , KC_PDOT, KC_PEQL, XXXXXXX
   ),
 
   [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL ,
+    KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
     KC_PIPE, _______, _______, _______, _______, _______, _______, KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_EQL , KC_LCBR, KC_RCBR, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
