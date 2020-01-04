1a61f08392c217d4b8f6d12664a8626d4f279a54
Juan Pumarino <juan@scribd.com>
2020-01-04 12:16:12 -0800

    Single layer experiment

---
+++ keyboards/preonic/keymaps/juan_single_layer/keymap.c
@@ -0,0 +1,101 @@
+#include QMK_KEYBOARD_H
+
+enum preonic_layers {
+  _BASE,
+  _NUMBR,
+  _RAISE,
+  _ADJUST,
+  _SPECIAL,
+};
+
+#define NUMBR MO(_NUMBR)
+#define RAISE MO(_RAISE)
+#define SPECIAL MO(_SPECIAL)
+
+#define CTR_ESC CTL_T(KC_ESC)
+#define HYP_TAB HYPR_T(KC_TAB)
+#define LWR_TAB LT(_NUMBR, KC_TAB)
+#define SFT_ENT SFT_T(KC_ENT)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  // [_BASE] = LAYOUT_preonic_grid(
+  //   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //   HYP_TAB, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+  //   CTR_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+  //   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_ENT,
+  //   NUMBR  , SPECIAL, KC_LALT, KC_LGUI, NUMBR  , KC_SPC , KC_SPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
+  // ),
+
+  [_BASE] = LAYOUT_preonic_grid(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+    CTR_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_ENT,
+    KC_HYPR, SPECIAL, KC_LALT, KC_LGUI, RAISE  , KC_SPC , KC_SPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
+  ),
+
+  // [_BASE] = LAYOUT_preonic_grid(
+  //   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //   LWR_TAB, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+  //   CTR_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+  //   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_ENT,
+  //   KC_HYPR, SPECIAL, KC_LALT, KC_LGUI, RAISE  , KC_SPC , KC_SPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
+  // ),
+
+  [_NUMBR] = LAYOUT_preonic_grid(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_PPLS, KC_7   , KC_8   , KC_9   , _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_PMNS, KC_4   , KC_5   , KC_6   , _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_PAST, KC_1   , KC_2   , KC_3   , KC_PENT,
+    _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_0   , KC_PDOT, KC_PEQL, XXXXXXX
+  ),
+
+  // [_NUMBR] = LAYOUT_preonic_grid(
+  //   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  //   _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
+  //   _______, _______, _______, _______, _______, _______, KC_PAST, KC_4   , KC_5   , KC_6   , KC_PPLS, _______,
+  //   _______, _______, _______, _______, _______, _______, KC_PSLS, KC_1   , KC_2   , KC_3   , KC_PMNS, KC_PENT,
+  //   _______, _______, _______, _______, _______, KC_PDOT, KC_PDOT, KC_0   , _______, _______, _______, _______
+  // ),
+
+  // [_NUMBR] = LAYOUT_preonic_grid(
+  //   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  //   KC_GRV , _______, _______, _______, _______, _______, KC_7   , KC_8   , KC_9   , KC_PEQL, _______, _______,
+  //   KC_PIPE, _______, _______, _______, _______, _______, KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PAST, _______,
+  //   _______, _______, _______, _______, _______, _______, KC_1   , KC_2   , KC_3   , KC_PMNS, KC_PSLS, KC_PENT,
+  //   _______, _______, _______, _______, _______, KC_0   , KC_0   , KC_PDOT, _______, _______, _______, _______
+  // ),
+
+  [_RAISE] = LAYOUT_preonic_grid(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_TILD, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
+    KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_PIPE,
+    KC_BSLS, _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_UNDS, KC_EQL , KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
+  [_ADJUST] = LAYOUT_preonic_grid(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, RESET  , DEBUG  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+  ),
+
+  [_SPECIAL] = LAYOUT_preonic_grid(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+  ),
+
+};
+
+uint32_t layer_state_set_user(uint32_t state) {
+  return update_tri_layer_state(state, _NUMBR, _RAISE, _ADJUST);
+}
+
+
+
