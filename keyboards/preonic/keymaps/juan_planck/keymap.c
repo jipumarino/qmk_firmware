58c977e0c2ef5cb233ea0c6889b369d1bf23a674
Juan Pumarino <juan@scribd.com>
2020-01-08 16:18:30 -0800

    Meh

--- keyboards/preonic/keymaps/juan_planck/keymap.c
+++ keyboards/preonic/keymaps/juan_planck/keymap.c
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_REALLY] = LAYOUT_preonic_grid(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
-    _______, KC_PIPE, KC_LCBR, KC_RCBR, KC_EQL , KC_MINS, KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, _______,
+    KC_PIPE, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
+    _______, _______, KC_LCBR, KC_RCBR, KC_EQL , KC_MINS, KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
